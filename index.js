7e0668fa61f15727af9b6477d605cdce72982152
Matt DuBois <mattduboismatt@gmail.com>
2016-06-23 14:11:10 -0500

    render single item by id

--- index.js
+++ index.js
@@ -16,14 +16,41 @@ class Item extends React.Component {
   }
 };
 
+Item = Relay.createContainer(Item, {
+  fragments: {
+    store: () => Relay.QL`
+      fragment on HackerNewsAPI {
+        item(id: 8863) {
+          title,
+          score,
+          url
+          by {
+            id
+          }
+        }
+      }
+    `,
+  },
+});
+
+class HackerNewsRoute extends Relay.Route {
+  static routeName = 'HackerNewsRoute';
+  static queries = {
+    store: ((Component) => {
+      return Relay.QL`
+      query root {
+        hn { ${Component.getFragment('store')} },
+      }
+    `}),
+  };
+}
+
+Relay.injectNetworkLayer(
+  new Relay.DefaultNetworkLayer('https://www.graphqlHub.com/graphql')
+);
+
 let mountNode = document.getElementById('container');
-let item = {
-  id  : '1337',
-  url : 'http://google.com',
-  title : 'Google',
-  score : 100,
-  by : { id : 'clay'}
-};
-let store = { item };
-let rootComponent = <Item store={store} />;
+let rootComponent = <Relay.RootContainer
+  Component={Item}
+  route={new HackerNewsRoute()} />;
 ReactDOM.render(rootComponent, mountNode);
