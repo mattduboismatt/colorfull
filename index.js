3fe608fc2247765409b7143a19e26866b180d5f9
Matt DuBois <mattduboismatt@gmail.com>
2016-06-23 13:46:51 -0500

    fake react component

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-alert("made it!");
+let React    = require('react');
+let ReactDOM = require('react-dom');
+
+class Item extends React.Component {
+  render() {
+    let item = this.props.store.item;
+
+    return (
+      <div>
+        <h1><a href={item.url}>{item.title}</a></h1>
+        <h2>{item.score} - {item.by.id}</h2>
+        <hr />
+      </div>
+    );
+  }
+};
+
+let mountNode = document.getElementById('container');
+let item = {
+  id  : '1337',
+  url : 'http://google.com',
+  title : 'Google',
+  score : 100,
+  by : { id : 'clay'}
+};
+let store = { item };
+let rootComponent = <Item store={store} />;
+ReactDOM.render(rootComponent, mountNode);
