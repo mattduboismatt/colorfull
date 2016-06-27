811064ffa94e7f06db4ab4252e7b5912d5e62c1b
Matt DuBois <mattduboismatt@gmail.com>
2016-06-27 11:17:06 -0500

    start of reddit

--- index.js
+++ index.js
@@ -2,99 +2,81 @@ let React    = require('react');
 let ReactDOM = require('react-dom');
 let Relay    = require('react-relay');
 
-class Item extends React.Component {
+class Link extends React.Component {
   render() {
-    let item = this.props.store;
+    let link = this.props.store;
 
     return (
       <div>
-        <h1><a href={item.url}>{item.title}</a></h1>
-        <h2>{item.score} - {item.by.id}</h2>
-        <hr />
+        <h3><a href={link.url}>{link.title}</a></h3>
+        <p>score: {link.score} - comments: {link.numComments} - author: {link.author.username}</p>
+        <hr/>
       </div>
     );
   }
 };
 
-class TopItems extends React.Component {
-  _onChange(ev) {
-    let storyType = ev.target.value;
-    this.setState({ storyType });
-    this.props.relay.setVariables({
-      storyType
-    });
-  }
-
+class Subreddit extends React.Component {
   render() {
-    let items = this.props.store.stories.map(
-      (store, idx) => <Item store={store} key={idx} />
+    let topListings = this.props.store.topListings.map(
+      (store, idx) => <Link store={store} key={idx} />
     );
-
-    let variables = this.props.relay.variables;
-
-    // To reduce the perceived lag
-    // There are less crude ways of doing this, but this works for now
-    let currentStoryType = (this.state && this.state.storyType) || variables.storyType;
-
     return <div>
-      <select onChange={this._onChange.bind(this)} value={currentStoryType}>
-        <option value="top">Top</option>
-        <option value="new">New</option>
-        <option value="ask">Ask HN</option>
-        <option value="show">Show HN</option>
-      </select>
-      { items }
+      <h1>Links</h1>
+      { topListings }
     </div>;
   }
 };
 
-Item = Relay.createContainer(Item, {
+Link = Relay.createContainer(Link, {
   fragments: {
     store: () => Relay.QL`
-      fragment on HackerNewsItem {
-        id,
+      fragment on RedditLink {
+        url,
         title,
         score,
-        url
-        by {
-          id
+        numComments,
+        author {
+          username
         }
       }
     `,
   },
 });
 
-TopItems = Relay.createContainer(TopItems, {
-  initialVariables: {
-    storyType: "top"
-  },
+
+Subreddit = Relay.createContainer(Subreddit, {
   fragments: {
     store: () => Relay.QL`
-      fragment on HackerNewsAPI {
-        stories(storyType: $storyType) { ${Item.getFragment('store')} },
+      fragment on RedditAPI {
+        subreddit(name: "dogs") {
+          topListings(limit: 20) { ${Link.getFragment('store')} },
+        }
       }
     `,
   },
 });
 
-class HackerNewsRoute extends Relay.Route {
-  static routeName = 'HackerNewsRoute';
+
+class RedditRoute extends Relay.Route {
+  static routeName = 'RedditRoute';
   static queries = {
     store: ((Component) => {
+      // Component is our Subreddit
       return Relay.QL`
       query root {
-        hn { ${Component.getFragment('store')} },
+        reddit {
+          subreddit(name: "dogs") { ${Component.getFragment('store')} },
+        }
       }
     `}),
   };
-}
+};
 
 Relay.injectNetworkLayer(
-  new Relay.DefaultNetworkLayer('https://www.graphqlHub.com/graphql')
+  new Relay.DefaultNetworkLayer('http://www.GraphQLHub.com/graphql')
 );
 
 let mountNode = document.getElementById('container');
-let rootComponent = <Relay.RootContainer
-  Component={TopItems}
-  route={new HackerNewsRoute()} />;
+let rootComponent = <Relay.RootContainer Component={Subreddit} route={new RedditRoute()} />;
 ReactDOM.render(rootComponent, mountNode);
